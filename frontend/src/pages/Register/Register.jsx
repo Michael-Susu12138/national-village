3d1790e0140f6e686d1fab2748d338016d048ba0
Christ <hc3539@nyu.edu>
2024-04-17 13:32:54 -0400

    register v2.1

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./Register.css";
+import React, { useState } from "react";
 
 const Register = () => {
   return (
@@ -108,158 +109,15 @@ const Register = () => {
                         By continuing to create an account, you agree to
                         National Village's Terms & Conditions and Privacy
                         Policy.
-                        {/* <a
-                          href="https://accounts.studentbeans.com/us/info/terms"
-                          target="_blank"
-                          style="color: rgb(101, 35, 227); text-decoration: underline;"
-                          rel="noreferrer"
-                        >
-                          Terms &amp; Conditions
-                        </a>
-                        and
-                        <a
-                          href="/us/info/privacy"
-                          target="_blank"
-                          style="color: rgb(101, 35, 227); text-decoration: underline;"
-                        >
-                          Privacy Policy
-                        </a>
-                        . */}
                       </span>
                     </p>
                   </div>
                 </form>
-
-                {/* <button
-                  aria-label="Sign up with Facebook"
-                  className="css-2sstpbq"
-                >
-                  <svg
-                    aria-hidden="true"
-                    width="18"
-                    height="18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    style="position: absolute; top: 16px; left: 16px;"
-                  >
-                    <mask
-                      id="a"
-                      maskUnits="userSpaceOnUse"
-                      x="0"
-                      y="0"
-                      width="18"
-                      height="18"
-                    >
-                      <path d="M0 0h18v17.89H0V0z" fill="#fff"></path>
-                    </mask>
-                    <g mask="url(#a)">
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M18 9a9 9 0 1 0-10.407 8.89v-6.289H5.308V9h2.285V7.017c0-2.256 1.344-3.502 3.4-3.502.984 0 2.015.176 2.015.176v2.215h-1.135c-1.118 0-1.467.694-1.467 1.405V9h2.496l-.399 2.601h-2.097v6.29C14.709 17.215 18 13.492 18 9z"
-                        fill="#1877F2"
-                      ></path>
-                    </g>
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="m12.503 11.601.4-2.601h-2.497V7.31c0-.711.348-1.405 1.467-1.405h1.135V3.69s-1.03-.176-2.015-.176c-2.056 0-3.4 1.246-3.4 3.502V9H5.308V11.6h2.285v6.29a9.068 9.068 0 0 0 2.813 0V11.6h2.097z"
-                      fill="#fff"
-                    ></path>
-                  </svg>
-                </button>
-                <button
-                  aria-label="Sign up with Google"
-                  className="css-2sstpbq"
-                >
-                  <svg
-                    aria-hidden="true"
-                    width="18"
-                    height="18"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    style="position: absolute; top: 16px; left: 16px;"
-                  >
-                    <g fill="#fff" fillRule="evenodd" clipRule="evenodd">
-                      <path
-                        d="M17.64 9.205c0-.639-.057-1.252-.164-1.841H9v3.481h4.844a4.14 4.14 0 0 1-1.796 2.716v2.259h2.908c1.702-1.567 2.684-3.875 2.684-6.615z"
-                        fill="#4285F4"
-                      ></path>
-                      <path
-                        d="M9 18c2.43 0 4.467-.806 5.956-2.18l-2.908-2.259c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 0 0 9 18z"
-                        fill="#34A853"
-                      ></path>
-                      <path
-                        d="M3.964 10.71A5.41 5.41 0 0 1 3.682 9c0-.593.102-1.17.282-1.71V4.958H.957a9.005 9.005 0 0 0 0 8.084l3.007-2.332z"
-                        fill="#FBBC05"
-                      ></path>
-                      <path
-                        d="M9 3.58c1.321 0 2.508.454 3.44 1.345l2.582-2.58C13.463.891 11.426 0 9 0A8.997 8.997 0 0 0 .957 4.958L3.964 7.29C4.672 5.163 6.656 3.58 9 3.58z"
-                        fill="#EA4335"
-                      ></path>
-                    </g>
-                  </svg>
-                </button>
-                <button aria-label="Sign up with Apple" className="css-2sstpbq">
-                  <svg
-                    aria-hidden="true"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    style="position: absolute; top: 16px; left: 16px;"
-                  >
-                    <path
-                      d="M15.637 7.214c-1.575 0-2.24.773-3.337.773-1.125 0-1.983-.768-3.347-.768-1.336 0-2.76.839-3.665 2.269-1.27 2.015-1.055 5.812 1.003 9.046.736 1.158 1.72 2.457 3.009 2.47h.023c1.12 0 1.454-.754 2.996-.763h.023c1.519 0 1.824.759 2.94.759h.023c1.289-.014 2.325-1.453 3.06-2.606.53-.83.727-1.247 1.135-2.185-2.977-1.162-3.455-5.503-.51-7.167-.9-1.158-2.162-1.828-3.352-1.828z"
-                      fill="#000"
-                    ></path>
-                    <path
-                      d="M15.29 3c-.937.066-2.029.68-2.671 1.481-.582.727-1.06 1.805-.872 2.85h.075c.998 0 2.02-.619 2.616-1.41.576-.755 1.012-1.824.853-2.921z"
-                      fill="#000"
-                    ></path>
-                  </svg>
-                </button> */}
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      {/* <div style="position: static; overflow: auto; background-color: rgb(246, 247, 249);">
-        <div className="css-1ibvymd">
-          <div className="css-1gap6ip">
-            <footer className="css-1efjo2a">
-              <div style="width: 1250px; max-width: 100%; min-width: 320px; margin: 0px auto; padding: 0px;">
-                <nav style="padding: 0px 0.5em;">
-                  <ul style="overflow: auto; padding: 0px; margin: 2.5em 0px;">
-                    <li style="white-space: nowrap; float: left; margin-left: 0px; list-style: none;">
-                      <a role="button" tabIndex="0" className="css-x7e2ps">
-                        Support
-                      </a>
-                    </li>
-                    <li style="white-space: nowrap; float: left; margin-left: 1.625em; list-style: disc;">
-                      <a role="button" tabIndex="0" className="css-x7e2ps">
-                        Student Beans
-                      </a>
-                    </li>
-                    <li style="white-space: nowrap; float: left; margin-left: 1.625em; list-style: disc;">
-                      <a role="button" tabIndex="0" className="css-x7e2ps">
-                        Privacy Policy
-                      </a>
-                    </li>
-                    <li style="white-space: nowrap; float: left; margin-left: 1.625em; list-style: disc;">
-                      <a role="button" tabIndex="0" className="css-x7e2ps">
-                        Terms &amp; Conditions
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </footer>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
