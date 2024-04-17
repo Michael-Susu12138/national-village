ed06873cb38b93fa0741c92b0828ce4090e6b1bd
Michael-Susu12138 <78223776+Michael-Susu12138@users.noreply.github.com>
2024-04-17 17:52:07 -0400

    added detail page, introduce google map api

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,7 +1,13 @@
 import "./Card.css";
+import { useNavigate } from "react-router-dom";
 
 const Card = (props) => {
   // name, discount, summary, rating, location, tags, imgURL;
+  const navigate = useNavigate();
+  const navigateToDestination = (event) => {
+    event.preventDefault(); // Prevent the default anchor behavior
+    navigate("/detail", { state: props });
+  };
 
   return (
     <div className="container">
@@ -20,65 +26,22 @@ const Card = (props) => {
           <h4>{props.name}</h4>
           <p>{props.summary}</p>
           <div className="user">
-            <img
-              src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo"
-              alt="user"
-            />
+            <a href="#" onClick={navigateToDestination}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                <path
+                  fill="#f9064f"
+                  d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"
+                />
+              </svg>
+            </a>
+
             <div className="user-info">
-              <h5>User 1</h5>
-              <small>2h ago</small>
+              <h5>{props.location}</h5>
+              {/* <small>2h ago</small> */}
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="card">
-        <div className="card-header">
-          <img
-            src="https://www.newsbtc.com/wp-content/uploads/2020/06/mesut-kaya-LcCdl__-kO0-unsplash-scaled.jpg"
-            alt="ballons"
-          />
-        </div>
-        <div className="card-body">
-          <span className="tag tag-purple">Popular</span>
-          <h4>How to Keep Going When You Don’t Know What’s Next</h4>
-          <p>
-            The future can be scary, but there are ways to deal with that fear.
-          </p>
-          <div className="user">
-            <img
-              src="https://lh3.googleusercontent.com/ogw/ADGmqu8sn9zF15pW59JIYiLgx3PQ3EyZLFp5Zqao906l=s32-c-mo"
-              alt="user"
-            />
-            <div className="user-info">
-              <h5>User 2</h5>
-              <small>Yesterday</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-header">
-          <img
-            src="https://images6.alphacoders.com/312/thumb-1920-312773.jpg"
-            alt="city"
-          />
-        </div>
-        <div className="card-body">
-          <span className="tag tag-pink">Design</span>
-          <h4>10 Rules of Dashboard Design</h4>
-          <p>Dashboard Design Guidelines</p>
-          <div className="user">
-            <img
-              src="https://studyinbaltics.ee/wp-content/uploads/2020/03/3799Ffxy.jpg"
-              alt="user"
-            />
-            <div className="user-info">
-              <h5>User 3</h5>
-              <small>1w ago</small>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
